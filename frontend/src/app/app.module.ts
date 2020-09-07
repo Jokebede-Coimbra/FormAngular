c123c1ae3de04bc0cab992f35be812c6171af9f5
Coimbra <44805096+Jokebede-Coimbra@users.noreply.github.com>
2020-09-07 08:53:30 -0300

    update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,3 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 
@@ -6,38 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
-import {MatSidenavModule  } from '@angular/material/sidenav';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import {MatCardModule} from '@angular/material/card';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { VioletaDirective } from './directives/violeta.directive';
-import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
-import {FormsModule  } from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
 import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { VioletaDirective } from './directives/violeta.directive';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ForDirective } from './directives/for.directive';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
-
-
 
 @NgModule({
   declarations: [
@@ -47,13 +46,13 @@ registerLocaleData(localePt);
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    VioletaDirective,
-    ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    VioletaDirective,
+    ForDirective,
     ProductUpdateComponent,
-    ProductDeleteComponent
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
@@ -66,16 +65,16 @@ registerLocaleData(localePt);
     MatButtonModule,
     MatSnackBarModule,
     HttpClientModule,
-    MatFormFieldModule,
     FormsModule,
+    MatFormFieldModule,
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
   ],
   providers: [{
-  provide: LOCALE_ID,
-  useValue: 'pt-BR'
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
   }],
   bootstrap: [AppComponent]
 })
